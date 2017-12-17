4ffdfcb9b5164f28995f1fa96be40104c13b7266
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-12-17 20:35:11 +0530

    Fixing set env

--- 1-helloboot/set-env.sh
+++ 1-helloboot/set-env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export SPRING_HOME="$HOME/spring-1.5.9.RELEASE"
-export $PATH="$SPRING_HOME/bin:$PATH"
+export SPRING_HOME="${HOME}/spring-1.5.9.RELEASE"
+export PATH="${SPRING_HOME}/bin:${PATH}"
+echo "Using spring cli"
+spring version
