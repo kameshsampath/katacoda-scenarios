b6ba571f1fca75798a5336a0e80286eaaa5287f3
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-12-17 20:39:16 +0530

    Fixing scripts to download spring cli

--- 1-helloboot/env-init.sh
+++ 1-helloboot/env-init.sh
@@ -1,4 +1,2 @@
 # ssh root@host01 "git --git-dir=/root/projects/helloboot/.git --work-tree=/root/projects/helloboot pull"
 ssh root@host01 "yum -y install wget tree"
-ssh root@host01 "wget https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/1.5.9.RELEASE/spring-boot-cli-1.5.9.RELEASE-bin.tar.gz"
-ssh root@host01 "tar -zxvf spring-boot-cli-1.5.9.RELEASE-bin.tar.gz"
--- 1-helloboot/set-env.sh
+++ 1-helloboot/set-env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+wget https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/1.5.9.RELEASE/spring-boot-cli-1.5.9.RELEASE-bin.tar.gz
+tar -zxf spring-boot-cli-1.5.9.RELEASE-bin.tar.gz
 export SPRING_HOME="${HOME}/spring-1.5.9.RELEASE"
 export PATH="${SPRING_HOME}/bin:${PATH}"
 echo "Using spring cli"
